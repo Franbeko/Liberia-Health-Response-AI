e2d359fcf7155493324f4e68501956bab041d338
Francis Kojo Haizel <franciskhhaizel@gmail.com>
2025-06-09 11:58:49 +0000

    helper.py updated

--- src/helper.py
+++ src/helper.py
@@ -3,27 +3,31 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
-#Extracted Data From the PDF File
+# Extracted Data From the PDF File
 def load_pdf_file(data):
-    loader= DirectoryLoader(data,
-                            glob="*.pdf",
-                            loader_cls=PyPDFLoader)
-    
-    documents=loader.load()
-
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+    documents = loader.load()
     return documents
 
 
-
-#Split the Data into Text Chunks
+# Split the Data into Text Chunks
 def text_split(extracted_data):
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-    text_chunks=text_splitter.split_documents(extracted_data)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+    text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
 
 
-
-#Download the Embeddings from Hugging Face
-def download_hugging_face_embeddings(model_name="sentence-transformers/paraphrase-albert-small-v2"):
-    from langchain_huggingface import HuggingFaceEmbeddings
-    return HuggingFaceEmbeddings(model_name=model_name)
+# Get Hugging Face Embeddings
+def get_embeddings():
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'normalize_embeddings': False}
+    )
